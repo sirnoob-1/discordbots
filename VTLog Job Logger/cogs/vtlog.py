29d68c1b3067e89672cd8d7afdc317df966354f5
sirnoob_1 <102556049+sirnoob-1@users.noreply.github.com>
2022-10-07 16:35:43 +0100

    Initial Commit

---
+++ VTLog Job Logger/cogs/vtlog.py
@@ -0,0 +1,202 @@
+#━━━━━━━━━━━━━━━━━━━━━━━ ❯ VTLog Job Logger Imports // Cog: VTLOG ❮ ━━━━━━━━━━━━━━━━━━━━━━━
+
+#-Module Imports-
+import datetime
+import nextcord
+import requests
+import calendar
+import time
+
+#-Class Imports-
+from nextcord.ext import commands, tasks
+
+#- VtlogHandler-
+class VtlogHandler(commands.Cog):
+    """A class for handling all Vtlog
+        events. This Cog is utilised
+        by running a 5 minute event timer
+        and checking for inconsistencies
+        within locally stored data and API"""
+
+    #-__Init__-
+    def __init__(self, vtlogClient):
+        self.vtlogClient = vtlogClient
+
+    #────────────────────》 Task Loops《────────────────────
+
+    #-Timer-
+    @tasks.loop(seconds = 30)
+    async def timer(self):
+        print('-------Starting Job Cycle-------')
+        startTime = int(time.time())
+
+
+        apiData = await self.fetch_api_data()
+        print('Job Logger -- [SUCCESS] VTLOG Data Fetched')
+
+
+        if apiData == False:
+            print('Job Logger -- [INFO] No new entries - returning')
+            self.vtlogClient.lastAPI = int(time.time())
+            print('Job Logger -- [INFO] Updated lastAPI timestamp.')
+            return
+
+        await self.update_member_cache()
+
+        memberCache = self.vtlogClient.memberCache
+
+        apiEmbedList = []
+
+        print('Job Logger -- [SUCCESS] Starting Job Entry Loop')
+        for jobEntry in apiData:
+
+
+            for member in memberCache['response']['members']:
+
+                if member['username'] == jobEntry['username']:
+                    break
+
+            print('Job Logger -- [SUCCESS] Found new job entry for ' + member['username'])
+
+            try:
+                jobData = jobEntry["data"]
+                jobDest = jobData["destination"]
+                jobSource = jobData["source"]
+                jobTruck = jobData["truck"]
+                jobCargo = jobData["cargo"]
+                jobFinance = jobData["finance"]
+                jobDate = calendar.day_name[datetime.date.today().weekday()][0:3] + ' ' + datetime.date.today().strftime("%B %d, %Y")
+
+            except:
+                print('Job Logger -- [FAIL] DATA BAD - Breaking loop')
+                break
+
+            print('Job Logger -- [SUCCESS] DATA OK - Constructing Embed')
+
+            jobEmbed = nextcord.Embed(title = 'Job Delivered | Result: ' + str(jobFinance["total_profit"]) + ' credits', url = f'https://vtlog.net/job/{jobEntry["job_id"]}', color = 0x0390fc)
+
+            jobEmbed.add_field(name = '📅 Date', value = jobDate + '**　　　　**') #- Expanding the first column to create a seperated look
+            jobEmbed.add_field(name = '🗺️ Source', value = jobSource["company"] + ' - ' + jobSource["city"])
+            jobEmbed.add_field(name = '👉 Destination', value = jobDest["company"] + ' - ' + jobDest["city"])
+            jobEmbed.add_field(name = '🚚 Vehicle', value = jobTruck["brand"] + ' ' + jobTruck["model"] + ' (' + jobTruck["license_plate"] + ')')
+            jobEmbed.add_field(name = '👋 Departed', value = f'<t:{jobSource["departure"]}:t>')
+            jobEmbed.add_field(name = '🌤️ Arrived', value = f'<t:{jobDest["arrival"]}:t>')
+            jobEmbed.add_field(name = '↔️ Distance', value = jobData["distance_driven"])
+            jobEmbed.add_field(name = '🚨 Cargo', value = jobCargo["name"])
+            jobEmbed.add_field(name = '🏋️‍♂️ Weight', value = str(jobCargo["mass"]) + 'kg')
+            jobEmbed.add_field(name = '⏰ Duration', value = str(datetime.timedelta(seconds = jobDest["arrival"] - jobSource["departure"])))
+            jobEmbed.add_field(name = '🌎 Fuel Economy', value = round(int(jobData["fuel_consumed"]) / jobData["distance_driven"] * 100, 2)  + ' L / 100km')
+            jobEmbed.add_field(name = '⛽ Fuel', value = str(jobData["fuel_consumed"]) + 'L')
+            jobEmbed.add_field(name = '💵 Profit', value = str(jobFinance["total_profit"]) + ' Credits')
+            jobEmbed.add_field(name = '📥 Earned Credits', value = jobFinance["total_income"])
+            jobEmbed.add_field(name = '📤 Lost Credits', value = jobFinance["total_expenses"])
+
+            jobEmbed.set_footer(text = 'Logbook', icon_url = 'https://i.imgur.com/l98wY7X.png')
+            jobEmbed.set_author(icon_url = member['avatar'], name = member['username'], url = f'https://vtlog.net/profile/{member["steam_id"]}')
+            jobEmbed.timestamp = datetime.datetime.fromtimestamp(int(jobDest["arrival"]))
+
+            if member['discord_id'] == 0:
+                member['discord_id'] == ' '
+
+            apiEmbedList.append([int(jobDest["arrival"]), jobEmbed, ('<@' + str(member["discord_id"]) + '>') if str(member["discord_id"]) != "0" else False])
+
+            print('Job Logger -- [SUCCESS] DATA OK - Embed entry Queued')
+
+        print('Job Logger -- [SUCCESS] Exited Job Entry Loop')
+
+        apiEmbedList.sort(key=lambda x: int(x[1].fields[5].value.strip(' ').strip('<t:').strip(':t>')))
+
+        print('Job Logger -- [SUCCESS] Data sorted - Exporting')
+
+        logGuild = self.vtlogClient.get_guild() #-Set
+        postChannel = logGuild.get_channel() #-Set
+
+        for jobArray in apiEmbedList:
+
+            await postChannel.send(embed = jobArray[1], content = jobArray[2] if jobArray[2] != False else '')
+            print('Job Logger -- [SUCCESS] Published entry for ' + jobArray[1].author.name)
+
+        print('Job Logger -- [SUCCESS] Job Cycle Exited - Completed in ' + str(int(time.time() - startTime)) + ' seconds')
+
+
+    #-MemberUpdater-
+    @tasks.loop(seconds = 600)
+    async def memberUpdater(self):
+
+        await self.update_member_cache()
+
+    #-TimerReviver-
+    @tasks.loop(seconds = 300)
+    async def timerReviver(self):
+
+        try:
+            self.timer.start()
+            self.memberUpdater.start()
+
+        except RuntimeError:
+            pass
+
+    #────────────────────》 Async Functions《────────────────────
+
+    #-Fetch_API_Data-
+    async def fetch_api_data(self):
+
+        callTime = int(time.time())
+
+        vtlogRequest = requests.get('https://api.vtlog.net/v3/companies/COMPANY_NUM/jobs') #-Set
+        vtlogJobs = vtlogRequest.json()['response']['jobs']
+        print('Job Logger -- [INFO] - VTlog API returned ' + str(vtlogRequest).strip('<>'))
+
+        newJobs = []
+
+
+        for jobObj in vtlogJobs:
+            
+            if int(jobObj['data']['destination']['arrival']) >= self.vtlogClient.lastAPI:
+
+                newJobs.append(jobObj)
+
+        self.vtlogClient.jobCache = vtlogJobs
+
+
+        if len(newJobs) == 0:
+            return False
+
+        self.vtlogClient.lastAPI = callTime
+        print('Job Logger -- [INFO] Updated lastAPI timestamp')
+
+        return newJobs
+
+    #-Update_Member_Cache-
+    async def update_member_cache(self):
+
+        self.vtlogClient.memberCache = requests.get('https://api.vtlog.net/v3/companies/COMPANY_NUM/members').json() #- Set
+        print('Job Logger -- [INFO] Member Cache Cycled')
+
+
+    #────────────────────》 Events《────────────────────
+
+    #On_Ready-
+    @commands.Cog.listener()
+    async def on_ready(self):
+
+        if not self.vtlogClient.hasConnected: #- If the bot has been started from file...
+            print(f'━━━━━━━━━━━━━━━━━━━━━━━━\n'
+                f'Logged in to Discord as {self.vtlogClient.user.display_name} // ID: {self.vtlogClient.user.id}\n'
+                f'API Version: {nextcord.__version__} // Nextcord\n'
+                f'Ping: {round(self.vtlogClient.latency * 1000)}ms\n'
+                f'Heartbeat: {str((round(self.vtlogClient._connection.heartbeat_timeout)))}ms\n'
+                f'Connected: {str(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))}'
+                '\n━━━━━━━━━━━━━━━━━━━━━━━━')
+
+            self.timer.start()
+            self.memberUpdater.start()
+            self.timerReviver.start()
+            print('Job Logger -- [SUCCESS] Timers Initiated')
+
+        else:
+            return print('{self.vtlogClient.user.display_name} Websocket reconnected ' + str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+
+
+def setup(vtlogClient):
+    vtlogClient.add_cog(VtlogHandler(vtlogClient))
---
+++ VTLog Job Logger/main.py
@@ -0,0 +1,51 @@
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ❯ VTlog Job Logger - Created by sirnoob_1#0001 ❮ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+'''
+ __      _________ _                        _       _       _                                 
+ \ \    / /__   __| |                      | |     | |     | |                                
+  \ \  / /   | |  | |     ___   __ _       | | ___ | |__   | |     ___   __ _  __ _  ___ _ __ 
+   \ \/ /    | |  | |    / _ \ / _` |  _   | |/ _ \| '_ \  | |    / _ \ / _` |/ _` |/ _ \ '__|
+    \  /     | |  | |___| (_) | (_| | | |__| | (_) | |_) | | |___| (_) | (_| | (_| |  __/ |   
+     \/      |_|  |______\___/ \__, |  \____/ \___/|_.__/  |______\___/ \__, |\__, |\___|_|   
+                                __/ |                                    __/ | __/ |          
+                               |___/                                    |___/ |___/   
+
+preifx
+
+'''
+
+#━━━━━━━━━━━━━━━━━━━━━━━ ❯ VTLog Job Logger Imports ❮ ━━━━━━━━━━━━━━━━━━━━━━━
+
+#-Module Imports-
+import nextcord
+import time
+import os
+
+#-Class Imports-
+from nextcord.ext import commands
+
+#━━━━━━━━━━━━━━━━━━━━━ ❯ VTLog Job Logger Declerations ❮ ━━━━━━━━━━━━━━━━━━━━━
+
+
+vtlogClient = commands.Bot(command_prefix = ["PREFIX"], case_insensitive = True, intents = nextcord.Intents.all()) #-Set
+vtlogClient.remove_command('help')
+
+vtlogClient.lastAPI = int(time.time()) #- An integer for the bot to filter newest results with
+vtlogClient.memberCache = {} #- Dictionary to hold attributes of VTLog users
+vtlogClient.jobCache = {} #- Dictionary to hold job cache, which holds the data of a previous API call, it looks for new entries by comparing it to updated data.
+
+vtlogClient.hasConnected = False #- Bool to prevent websocket reconnections from breaking the timers
+
+#━━━━━━━━━━━━━━━━━━━━━ ❯ VTLog Job Logger Startup (<> LIVE <>) ❮ ━━━━━━━━━━━━━━━━━━━━━
+
+if os.path.exists:
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            vtlogClient.load_extension(f'cogs.{filename[:-3]}')
+            print(f'Job Logger -- Loaded {filename[:-3]} --')
+
+else:
+    print("Job Logger -- ERROR -- Could not find extension directory!")
+
+#-Run-
+vtlogClient.run('INSERT TOKEN HERE') #-Set
